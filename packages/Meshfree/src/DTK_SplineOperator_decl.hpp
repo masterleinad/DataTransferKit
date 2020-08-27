ee3095180237e04142e7344828065b022b06b9fa
Daniel Arndt <arndtd@ornl.gov>
2020-08-27 11:41:39 -0400

    Add dummy implementation for Spline operator

---
+++ packages/Meshfree/src/DTK_SplineOperator_decl.hpp
@@ -0,0 +1,67 @@
+/****************************************************************************
+ * Copyright (c) 2012-2019 by the DataTransferKit authors                   *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the DataTransferKit library. DataTransferKit is     *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef DTK_SPLINE_OPERATOR_DECL_HPP
+#define DTK_SPLINE_OPERATOR_DECL_HPP
+
+#include <DTK_CompactlySupportedRadialBasisFunctions.hpp>
+#include <DTK_MultivariatePolynomialBasis.hpp>
+#include <DTK_PointCloudOperator.hpp>
+
+#include <Tpetra_CrsMatrix.hpp>
+
+#include <mpi.h>
+
+namespace DataTransferKit
+{
+
+/**
+ * This class implements a function reconstruction technique for arbitrary point
+ * cloud based on a moving least square discretization. In this method, support
+ * and subsequently the data transfer operator is constructed through solutions
+ * to local least square kernels defined by compactly supported radial basis
+ * functions.
+ *
+ * The class is templated on the DeviceType, the radial basis function
+ * (Wendland<0>, Wendland<2>, Wendland<4>, Wendland<6>, Wu<2>, Wu<4>,
+ * Buhmann<2>, Buhmann<3>, or Buhmann<4>) and polynonial basis (<Constant, DIM>,
+ * <Linear, DIM>, or <Quadratic, DIM>).
+ */
+template <typename DeviceType,
+          typename CompactlySupportedRadialBasisFunction = Wendland<0>,
+          typename PolynomialBasis = MultivariatePolynomialBasis<Linear, 3>>
+class SplineOperator : public PointCloudOperator<DeviceType>
+{
+    using ExecutionSpace = typename DeviceType::execution_space;
+
+  public:
+    SplineOperator(
+        MPI_Comm comm,
+        Kokkos::View<Coordinate const **, DeviceType> source_points,
+        Kokkos::View<Coordinate const **, DeviceType> target_points );
+
+    void
+    apply( Kokkos::View<double const *, DeviceType> source_values,
+           Kokkos::View<double *, DeviceType> target_values ) const override;
+
+  private:
+    MPI_Comm _comm;
+    unsigned int const _n_source_points;
+    Kokkos::View<int *, DeviceType> _offset;
+    Kokkos::View<int *, DeviceType> _ranks;
+    Kokkos::View<int *, DeviceType> _indices;
+    Kokkos::View<double *, DeviceType> _coeffs;
+    Tpetra::CrsMatrix<> _crs_matrix;
+};
+
+} // end namespace DataTransferKit
+
+#endif
---
+++ packages/Meshfree/src/DTK_SplineOperator_def.hpp
@@ -0,0 +1,146 @@
+/****************************************************************************
+ * Copyright (c) 2012-2019 by the DataTransferKit authors                   *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the DataTransferKit library. DataTransferKit is     *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef DTK_SPLINE_OPERATOR_DEF_HPP
+#define DTK_SPLINE_OPERATOR_DEF_HPP
+
+#include <ArborX.hpp>
+#include <DTK_DBC.hpp>
+#include <DTK_DetailsSplineOperatorImpl.hpp>
+#include <DTK_DetailsNearestNeighborOperatorImpl.hpp> // fetch
+
+namespace DataTransferKit
+{
+
+template <typename DeviceType, typename CompactlySupportedRadialBasisFunction,
+          typename PolynomialBasis>
+SplineOperator<DeviceType, CompactlySupportedRadialBasisFunction,
+                           PolynomialBasis>::
+    SplineOperator(
+        MPI_Comm comm,
+        Kokkos::View<Coordinate const **, DeviceType> source_points,
+        Kokkos::View<Coordinate const **, DeviceType> target_points )
+    : _comm( comm )
+    , _n_source_points( source_points.extent( 0 ) )
+    , _offset( "offset", 0 )
+    , _ranks( "ranks", 0 )
+    , _indices( "indices", 0 )
+    , _coeffs( "polynomial_coefficients", 0 )
+    , _crs_matrix(Teuchos::RCP<Tpetra::Map<> > {}, 0)
+{
+    DTK_REQUIRE( source_points.extent_int( 1 ) ==
+                 target_points.extent_int( 1 ) );
+    // FIXME for now let's assume 3D
+    DTK_REQUIRE( source_points.extent_int( 1 ) == 3 );
+
+    // Build distributed search tree over the source points.
+    ArborX::DistributedSearchTree<DeviceType> search_tree( _comm,
+                                                           source_points );
+    DTK_CHECK( !search_tree.empty() );
+
+    // For each target point, query the n_neighbors points closest to the
+    // target.
+    auto queries =
+        Details::SplineOperatorImpl<DeviceType>::makeKNNQueries(
+            target_points, PolynomialBasis::size );
+
+    // Perform the actual search.
+    search_tree.query( queries, _indices, _offset, _ranks );
+
+    // Retrieve the coordinates of all source points that met the predicates.
+    // NOTE: This is the last collective.
+    source_points = Details::NearestNeighborOperatorImpl<DeviceType>::fetch(
+        _comm, _ranks, _indices, source_points );
+
+    // Transform source points
+    source_points = Details::SplineOperatorImpl<
+        DeviceType>::transformSourceCoordinates( source_points, _offset,
+                                                 target_points );
+    target_points = Kokkos::View<Coordinate **, DeviceType>( "empty", 0, 0 );
+
+    // Build P (vandermonde matrix)
+    // P is a single 1D storage for multiple P_i matrices. Each matrix is of
+    // size (#source_points_for_specific_target_point, basis_size)
+    auto p =
+        Details::SplineOperatorImpl<DeviceType>::computeVandermonde(
+            source_points, PolynomialBasis() );
+
+    // To build the radial basis function, we need to define the radius of the
+    // radial basis function. Since we use kNN, we need to compute the radius.
+    // We only need the coordinates of the source points because of the
+    // transformation of the coordinates.
+    auto radius =
+        Details::SplineOperatorImpl<DeviceType>::computeRadius(
+            source_points, _offset );
+
+    // Build phi (weight matrix)
+    auto phi =
+        Details::SplineOperatorImpl<DeviceType>::computeWeights(
+            source_points, radius, CompactlySupportedRadialBasisFunction() );
+
+    // Build A (moment matrix)
+    auto a =
+        Details::SplineOperatorImpl<DeviceType>::computeMoments(
+            _offset, p, phi );
+
+    // TODO: it is computationally unnecessary to compute the pseudo-inverse as
+    // MxM (U*E^+*V) as it will later be just used to do MxV. We could instead
+    // return the (U,E^+,V) and do the MxV multiplication. But for now, it's OK.
+    auto t = Details::SplineOperatorImpl<DeviceType>::invertMoments(
+        a, PolynomialBasis::size );
+    auto inv_a = std::get<0>( t );
+
+    // std::get<1>(t) returns the number of undetermined system. However, this
+    // is not enough to know if we will lose order of accuracy. For example, if
+    // all the points are aligned, the system will be underdetermined. However
+    // this is not a problem if we found at least three points since this is
+    // enough to define a quadratic function. Therefore, not only we need to
+    // know the rank deficiency but also the dimension of the problem.
+
+    // NOTE: This assumes that the polynomial basis evaluated at {0,0,0} is
+    // going to be [1, 0, 0, ..., 0]^T.
+    _coeffs = Details::SplineOperatorImpl<
+        DeviceType>::computePolynomialCoefficients( _offset, inv_a, p, phi,
+                                                    PolynomialBasis::size );
+}
+
+template <typename DeviceType, typename CompactlySupportedRadialBasisFunction,
+          typename PolynomialBasis>
+void SplineOperator<
+    DeviceType, CompactlySupportedRadialBasisFunction, PolynomialBasis>::
+    apply( Kokkos::View<double const *, DeviceType> source_values,
+           Kokkos::View<double *, DeviceType> target_values ) const
+{
+    // Precondition: check that the source and the target are properly sized
+    DTK_REQUIRE( source_values.extent( 0 ) == _n_source_points );
+    DTK_REQUIRE( target_values.extent( 0 ) == _offset.extent( 0 ) - 1 );
+
+    // Retrieve values for all source points
+    source_values = Details::NearestNeighborOperatorImpl<DeviceType>::fetch(
+        _comm, _ranks, _indices, source_values );
+
+    // Apply A-1 (P^T phi)
+    auto new_target_values = Details::SplineOperatorImpl<
+        DeviceType>::computeTargetValues( _offset, _coeffs, source_values );
+
+    Kokkos::deep_copy( target_values, new_target_values );
+}
+
+} // end namespace DataTransferKit
+
+// Explicit instantiation macro
+#define DTK_SPLINE_OPERATOR_INSTANT( NODE )                      \
+    template class SplineOperator<typename NODE::device_type>;     \
+    template class SplineOperator<                                 \
+        typename NODE::device_type, Wendland<0>,                               \
+        MultivariatePolynomialBasis<Quadratic, 3>>;
+
+#endif
